bea0fdc1113ed541edf0b695bc274abe9ddd7534
Travis CI User <travis@example.org>
2020-01-08 09:55:38 +0000

    Updates

---
+++ precache-manifest.5a0c62f5c0977eaa04a2b92243ed5e3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca77c0e4401734eb3479dd9d615c4390",
+    "url": "/year-view-calendar/index.html"
+  },
+  {
+    "revision": "ea2138f35bef797298ec",
+    "url": "/year-view-calendar/static/css/main.88c060e0.chunk.css"
+  },
+  {
+    "revision": "663e90e8722e2c2db6b7",
+    "url": "/year-view-calendar/static/js/2.887885b8.chunk.js"
+  },
+  {
+    "revision": "ea2138f35bef797298ec",
+    "url": "/year-view-calendar/static/js/main.23a28605.chunk.js"
+  },
+  {
+    "revision": "2af3258796dc6695aaab",
+    "url": "/year-view-calendar/static/js/runtime~main.b55ea234.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/year-view-calendar/precache-manifest.19956fa440b08dbb765f93197aafb144.js"
+  "/year-view-calendar/precache-manifest.5a0c62f5c0977eaa04a2b92243ed5e3e.js"
 );
 
 self.addEventListener('message', (event) => {
